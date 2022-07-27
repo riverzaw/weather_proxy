84b120267135620d16166353ab29dd7c80f888ae
riverzaw <33205668+riverzaw@users.noreply.github.com>
2022-07-27 15:04:49 +0200

    initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/riverzaw/proxyWeather
+
+go 1.18
---
+++ proxy.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+)
+
+func main() {
+	url, err := url.Parse("https://api.darksky.net/")
+	if err != nil {
+		panic(err)
+	}
+	username := os.Getenv("METEOLOGIN")
+	password := os.Getenv("METEOPASS")
+	key := os.Getenv("DARKSKYKEY")
+
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	handler := func(h http.Handler) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			user, pass, _ := r.BasicAuth()
+			if user != username || pass != password {
+				http.Error(w, "Wrong user or password", http.StatusUnauthorized)
+				return
+			}
+			r.Host = url.Host
+			r.URL.Path = "/forecast/" + key + r.URL.Path
+			q := r.URL.Query()
+			q.Add("exclude", "minutely,alerts,flags")
+			r.URL.RawQuery = q.Encode()
+			h.ServeHTTP(w, r)
+		}
+	}
+	if err := http.ListenAndServe(":9034", handler(proxy)); err != nil {
+		panic(err)
+	}
+}
